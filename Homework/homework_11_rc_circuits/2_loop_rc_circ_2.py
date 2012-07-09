d9652a24e6e5b6ed59e5f2a94b725194e222f945
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-07-09 15:22:39 -0500

    update

--- Homework/homework_11_rc_circuits/2_loop_rc_circ_2.py
+++ Homework/homework_11_rc_circuits/2_loop_rc_circ_2.py
@@ -18,14 +18,14 @@ print( v/r_equiv_0)
 # 2)
 
 r_equiv_infty = r_1 + r_3 + r_4
-
-print( r_3 * v/r_equiv_infty * c/mu)
+q_infty = r_3 * v/r_equiv_infty * c
+print( q_infty/mu)
 
 # 3)
 
-t = 548.0
-q_0 = r_3 * v/r_equiv_infty * c
-print( q_0 * e**(-t/(r_4 * c)) /mu)
+t = 548.0 * mu
+q_0 = q_infty
+print( q_0 * e**(-t/((r_3 + r_2) * c)) /mu)
 
 # 4)
 i_1, i_2, i_3 = symbols('i_1 i_2 i_3')
@@ -34,3 +34,7 @@ print( solve( (v/r_equiv_0 - i_1,
         i_1 - i_2 - i_3,
         r_2 * i_2 - r_3 * i_3
         ), i_1, i_2, i_3))
+
+# 5)
+
+print( q_0 /((r_3 + r_2) * c))

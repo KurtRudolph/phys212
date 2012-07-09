21c65079265aa201747104431bb033379469b612
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-07-09 16:27:31 -0500

    update

--- Homework/homework_12_magnetism/motion_in_mag_field_1.py
+++ Homework/homework_12_magnetism/motion_in_mag_field_1.py
@@ -1,11 +1,13 @@
 from sympy import * 
 from mpmath import *
-
+mu = 10.0**(-6)
 m = 7.5 * 10.0**(-8)
 B = 1.9
+t = 481.0 * mu
 r = 0.5
-d = pi * r
+d = pi/2.0 * r
 
-t = d/v
 
-print( t)
+# 1)
+v = d/t
+print( v)

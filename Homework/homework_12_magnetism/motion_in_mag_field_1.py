3e7bbf7982fc3a3b671eca4e882c330e950dded4
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-07-10 23:44:33 -0500

    update

--- Homework/homework_12_magnetism/motion_in_mag_field_1.py
+++ Homework/homework_12_magnetism/motion_in_mag_field_1.py
@@ -11,3 +11,21 @@ d = pi/2.0 * r
 # 1)
 v = d/t
 print( v)
+
+# 2)
+ke = 1.0/2.0 * m * v**2
+t_1 = 160.3 * mu
+
+percent_d_traveled = t_1/t
+
+d * percent_d_traveled 
+
+q = symbols( 'q')
+
+q = solve( m * v/(q * B) - r, q)[0]
+
+print( q * v * B * percent_d_traveled)
+
+
+# 4)
+print(q/mu)

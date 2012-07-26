52f7ea6f2a63326719e03dac77f327dafd04d673
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-07-26 12:45:43 -0500

    update

---
+++ Homework/homework_24_polarization/polarization.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+const long double 
+  pi = 3.13159265,
+  mu_0 = 4 * pi * pow(10.0, -7),
+
+long double 
+  omega_1 = 60,
+  omega_2 = 130;
+
+printf("B = %32.16LE \n", B);
+
+.5 * I_0 *cos^2(theta_2 - theta_1) * cos(

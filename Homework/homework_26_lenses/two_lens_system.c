4122f975f5044be40ee245a0dea229b7e3f56755
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-08-01 15:40:43 -0500

    update

--- Homework/homework_26_lenses/two_lens_system.c
+++ Homework/homework_26_lenses/two_lens_system.c
@@ -5,9 +5,9 @@
 
 int main( ){
 
-double  x_2 = -.107,
-        y_2 = .049,
-        f   = -.254;
+double  x_2 = -.169,
+        y_2 = .021,
+        f   = -.422;
 
 //1)
 double x_1 = -1 / (1 / f - 1 / x_2);
@@ -18,8 +18,8 @@ double y_1 = x_1 * y_2 / x_2;
 printf( "y_1 = %lf\n", y_1 * 100);
 
 //3)
-double x_3 = -.1202;
-double f_converging = .0526;
+double x_3 = -.172;
+double f_converging = .0815;
 double x_4 = 1 / ( 1 / f_converging - 1 / (x_3 - x_1)) + x_3;
 printf( "x_4 = %lf\n", x_4 * 100);
 

3c7bef0560812754fc2648b0ac814ad7471be830
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-08-01 09:06:26 -0500

    update

---
+++ Homework/homework_26_lenses/single_lens_system.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.1415968
+
+int main( ){
+
+  double  x_1 = -.650,
+          x_2 = 1.32,
+          y_1 = .0206,
+          n   = 1.65;
+//1)
+  double f_lens = 1 / (-1 / x_1 + 1 / x_2);
+  printf( "f_lens = %lf\n", f_lens * 100);
+
+//2)
+  double y_2 = y_1 * x_2 / (-x_1);
+  printf( "y_2 = %lf\n", -y_2 * 100);
+
+//3)
+  double R = (n - 1) * f_lens;
+  printf( "R = %lf\n", R * 100);
+
+//4)
+  double x_1_new = -192;
+  double x_2_new = 1 / (1 / f_lens + 1 / x_1_new);
+  printf( "x_2_new = %lf\n", x_2_new * 100);
+
+return 0;
+}
---
+++ Homework/homework_26_lenses/two_lens_system.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <math.h>
+
+#define PI 3.1415968
+
+int main( ){
+
+double  x_2 = -.107,
+        y_2 = .049,
+        f   = -.254;
+
+//1)
+double x_1 = -1 / (1 / f - 1 / x_2);
+printf( "x_1 = %lf\n", x_1 * 100);
+
+//2)
+double y_1 = x_1 * y_2 / x_2;
+printf( "y_1 = %lf\n", y_1 * 100);
+
+//3)
+double x_3 = -.1202;
+double f_converging = .0526;
+double x_4 = 1 / ( 1 / f_converging - 1 / (x_3 - x_1)) + x_3;
+printf( "x_4 = %lf\n", x_4 * 100);
+
+
+return 0;
+}

b55100b282a75614a60dbfc14e6cec5af43a5558
Kurt Robert Rudolph <rudolph.kurt@rudyindustries.com>
2012-08-02 14:46:07 -0500

    update

---
+++ Homework/homework_27_mirrors/convex_mirror.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main( ){ 
+
+  double  x_2 = .307,
+          y_2 = .035,
+          f   = .52;
+
+  double x_1 = f * x_2 / ( x_2 - f);
+  printf( "x_1 = %lf\n", x_1 * 100);
+
+  double y_1 = -x_1 * y_2 / x_2;
+  printf( "y_1 = %lf\n", y_1 * 100);
+
+  double x_2_new = x_2 / 2;
+  double x_1_new = 1 / (1 / f - 1 / x_2_new);
+  printf( "x_1_%lf\n", x_1_new * 100);
+  
+
+  double y_1_new = -x_1_new * y_2 / x_2_new;
+  printf( "y_1_new = %lf\n", y_1_new * 100);
+
+return 0;
+}
